2417b796c3ffa67248b33b50b34b40be2c2829e5
Tharaka <stwijasinghe@gmail.com>
2020-07-28 05:01:30 +0530

    modify business layer

--- src/business/BusinessLogic.java
+++ src/business/BusinessLogic.java
@@ -1,4 +1,50 @@
 package business;
 
+import dao.DataLayer;
+import util.CustomerTM;
+import util.ItemTM;
+import util.OrderDetailTM;
+import util.OrderTM;
+
+import java.util.List;
+
 public class BusinessLogic {
+
+
+    public static List<CustomerTM> getAllCustomer(){
+        return DataLayer.getAllCustomer();
+    }
+
+    public static boolean saveCustomer(CustomerTM customer){
+        return DataLayer.saveCustomer(customer);
+    }
+
+    public static boolean deleteCustomer(String customerId){
+        return DataLayer.deleteCustomer(customerId);
+    }
+
+    public static boolean updateCustomer(CustomerTM customer){
+        return DataLayer.updateCustomer(customer);
+    }
+
+    public static List<ItemTM> getAllItems(){
+        return DataLayer.getAllItems();
+    }
+
+    public static boolean saveItem(ItemTM item){
+        return DataLayer.saveItem(item);
+    }
+
+    public static boolean deleteItem(String itemCode){
+        return DataLayer.deleteItem(itemCode);
+    }
+
+    public static boolean updateItem(ItemTM item){
+        return DataLayer.updateItem(item);
+    }
+
+    public static boolean placeOrder(OrderTM order, List<OrderDetailTM> orderDetails){
+        return DataLayer.placeOrder(order,orderDetails);
+    }
+
 }
